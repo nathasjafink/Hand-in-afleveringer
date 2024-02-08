3190b18b75f60aca5dd07c85ceaca08cfc93dbb7
nathasjafink <nathasja.nielsen@gmail.com>
2024-02-08 09:50:49 +0100

    exercises 5

--- app/src/main/java/com/example/hand_in_afleveringer/hand-in-1.kt
+++ app/src/main/java/com/example/hand_in_afleveringer/hand-in-1.kt
@@ -8,7 +8,8 @@ fun main () {
     minNumber(minNumbers)
     maxNumber(maxNumbers)
     calculateAvg()
-    checkCprNumber()
+    //checkCprNumber()
+    multiplesProgram()
 }
 
 /*
@@ -91,4 +92,26 @@ fun checkCprNumber () : Boolean {
 
     return !(day == null || month == null || day !in 1..31 || month !in 1..12)
 
+}
+
+/*
+5.
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print “Fizz” instead of the number
+and for the multiples of five print “Buzz”.
+For numbers which are multiples of both three and five print “FizzBuzz”.
+ */
+fun multiplesProgram () {
+    for (i in 1..100) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            println("FizzBuzz")
+        }
+        else if (i % 3 == 0) {
+            println("Fizz")
+        }
+        else if (i % 5 == 0) {
+            println("Buzz")
+        }
+        else println(i)
+    }
 }
