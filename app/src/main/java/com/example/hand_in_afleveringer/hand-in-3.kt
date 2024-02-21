a3f709b7940eb787e7f11704ca9992cbadc6a2c8
nathasjafink <nathasja.nielsen@gmail.com>
2024-02-21 08:12:48 +0100

    update

--- app/src/main/java/com/example/hand_in_afleveringer/hand-in-3.kt
+++ app/src/main/java/com/example/hand_in_afleveringer/hand-in-3.kt
@@ -1,2 +1,53 @@
 package com.example.hand_in_afleveringer
+// Main Function
+fun Main () {
 
+}
+
+/*
+1.
+Write a class: Article
+An article has an author and a title
+Create 5 articles, add them into an ArrayList and print them by overriding the .toString() method
+*/
+/*
+2.
+Create an interface called FastFood (with appropriate methods) and create a Sandwich class, a Pizza class and a class you decide that implements the FastFood interface.
+Add some different Fastfood objects to an array. Now iterate through that array and use some of the methods you have created above.
+*/
+/*
+3.
+Create a class that implements the following interface. Now create two objects using the class created
+interface Vehicle {
+    changeGear(int a);
+    speedUp(int a);
+    applyBrakes(int a);
+}
+*/
+/*
+4.
+Write a class: RedditPost
+A Redditpost has :
+A date of which is has been posted
+An author
+A balance of upvotes / downvotes
+A Title
+When a new instance of RedditPost is instantiated:
+The current date will be generated.
+The balance of upvotes and downvotes starts at 1.
+The title and author has to be provided by the constructor.
+Ensure all attributes are private, but accesible by getters & setters.
+Implement functionality such that redditposts can be sorted by upvotes/downvotes
+4.5
+Write a class: RedditFrontPage
+The RedditFrontPage has:
+A List of all RedditPosts
+A method in RedditFrontPage deletes a RedditPost from the list, by its index number
+
+ */
+
+/*
+5.
+Write a program that takes a list of words as input and prints the frequency of each word.
+Hint: Use a MutableMap
+ */
